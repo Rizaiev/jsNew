1199a3acd71ec9e57db433d78c6bf0e71845a129
Rizaiev <zooom2386@mail.ru>
2016-05-23 21:25:42 +0300

    price

---
+++ first/new.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Юзер on 23.05.2016.
+ */
+name = prompt("item call:");
+priceperitem = prompt("Get prise per item");
+count = prompt("Get count per item");
+alert ("Thank you for buying  " + name + "(s). " + "Total: " + priceperitem*count);
